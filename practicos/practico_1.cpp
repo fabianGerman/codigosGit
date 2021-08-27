6887ad4858dd15fddef628a56cecc390cfdff875
fabianGerman <fabianontiveros_7@hotmail.com>
2021-08-27 03:09:50 -0300

    hech

---
+++ practicos/practico_1.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<string.h>
+
+using namespace std;
+
+int main(){
+
+}
+
+void punto1(){
+    string mensaje;
+    cout<<"ingresar mensaje "<<endl;
+    cin>>mensaje;
+    cout<<mensaje<<endl;
+}
